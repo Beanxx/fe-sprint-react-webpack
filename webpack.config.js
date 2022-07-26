c50775f93a99c875f783146508b4c45f396ba877
Beanxx <tnqls991211@naver.com>
2022-07-26 15:14:02 +0900

    chore: delete webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "development",
@@ -42,6 +40,5 @@ module.exports = {
       template: path.resolve(__dirname, "./public/index.html"),
     }),
     new MiniCssExtractPlugin(),
-    new BundleAnalyzerPlugin(),
   ],
 };
