ab4663f0fa232e4df88c29c6fd488528382ac79a
Beanxx <tnqls991211@naver.com>
2022-07-26 14:54:46 +0900

    feat: react에 webpack 사용하여 배포하기

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+    runtimeChunk: "single",
+  },
+  // html-webpack-plugin 적용 부분
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+    new BundleAnalyzerPlugin(),
+  ],
+};
